6b3d5e0b144d29042d6e97e3e29ab168bbd2003c
brian107403 <brian107403@naver.com>
2017-12-05 16:41:26 +0900

    upload new file

--- machugi.c
+++ machugi.c
@@ -12,12 +12,12 @@ void machugi_menu(void){
 	char* day;
 	FILE *day_fp;
 	Wordbook *now = (Wordbook*)malloc(sizeof(Wordbook));
-	
+
 	//printf("파일명(일차):");
 	//scanf("%s",day);
 
 	day = userInputS(0, "파일명(일차) : ");
-	
+
 	int WbDay=atoi(day);
 	now = getNthWbPtr(WbDay);
 	//openWbFILE(now->id);
@@ -28,31 +28,26 @@ void machugi_menu(void){
 	//strcat(day,".dic");
 	/*
 	 * if((day_fp=fopen(day,"r"))==NULL){
-		printf("오류: 파일을 열수없습니다.\n");
-		exit(-1);
-	}
-	*/
+	 printf("오류: 파일을 열수없습니다.\n");
+	 exit(-1);
+	 }
+	 */
 
-	printf("출력방식(알파벳 순서대로:1, 무작위:2)");
-	scanf("%d",&type);
-	printf("check\n");
+	//scanf("%d",&type);
+	type=userInputN("출력방식(알파벳  순서대로:1, 무작위:2)");
 	machugi(type,now,WbDay);
 
 }
 void machugi(int type,Wordbook *now,int WbDay){  
-	printf("check\n");
 	int i;
 	int question[31];
-	printf("check\n");
 	char *day = now->id;
-	printf("check\n");
 
-	
-	printf("%s\n", now->id);
+
+	//printf("%s\n", now->id);
 
 	int len= WListLen(now->id);
-	
-	printf("%d",len);
+
 	for(i=0;i<len;i++)
 		question[i]=i;
 	question[i]=-1;
@@ -73,14 +68,17 @@ void machugi(int type,Wordbook *now,int WbDay){
 	int cnt_q=0,cnt_c=0,cnt_ic=0;//전체,맞은,틀린문제수
 	int num=0;
 	char *ans;
+	int c;// 엔터일때  메뉴로 나감
 	int q_num=0;//퀴즈번호
 	while(1){
 		Word * Wnow=getNthWPtr(WbDay,question[q_num]);
 		printf("%s->",Wnow->eng);
+
 		ans=userInputS(0,"");
 		if((strcmp(ans,".quit"))==0){
-			printf("당신의 점수는 %.2f 점입니다.",((float)cnt_c/cnt_q)*100);
-			break; 
+			printf("당신의 점수는 %.2f 점입니다.\n",((float)cnt_c/cnt_q)*100);
+			while( getchar() != '\n' ) ;
+			break;
 		}
 		if((strcmp(ans,Wnow->korDef[0])==0)||(strcmp(ans,Wnow->korDef[1])==0)||(strcmp(ans,Wnow->korDef[2])==0)){
 			printf("correct!\n");
@@ -94,3 +92,4 @@ void machugi(int type,Wordbook *now,int WbDay){
 		}
 	}
 }
+
